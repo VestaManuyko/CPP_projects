66eed854a5e90530a695d4d01b97481a235152cf
Vesta Manuyko <Vesta.manuyko@gmail.com>
2026-08-24 11:59:09 +0200

    added files for ex01 cpp06

---
+++ cpp06/ex01/Serializer.cpp
@@ -0,0 +1,12 @@
+#include "Serializer.hpp"
+
+Serializer::Serializer() {} ;
+
+Serializer::Serializer(const Serializer& other) {} ;
+
+Serializer& Serializer::operator=(const Serializer& other)
+{
+	return (*this);
+}
+
+Serializer::~Serializer() {} ;
---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,25 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <cstdint>
+
+struct Data
+{
+	/* data */
+};
+
+
+class Serializer
+{
+	private:
+    	Serializer();
+		Serializer(const Serializer& other); 
+		Serializer& operator=(const Serializer& other);
+		~Serializer();
+
+	public:
+		static uintptr_t	serialize(Data* ptr);
+		static Data*		deserialize(uintptr_t raw);
+};
+
+#endif
