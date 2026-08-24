a368ca9befc8803bce6e9f063a5a732502629f3c
Vesta Manuyko <Vesta.manuyko@gmail.com>
2026-08-24 17:10:13 +0200

    generate function done

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -1,4 +1,9 @@
 #include "Base.hpp"
+#include <cstdlib>
+#include <ctime>
+#include "A.cpp"
+#include "B.hpp"
+#include "C.hpp"
 
 Base::~Base()
 {
@@ -7,7 +12,27 @@ Base::~Base()
 
 Base * generate(void)
 {
-
+	srand(time(0));
+	int number = rand() % 3;
+	switch(number)
+	{
+	case 0:
+	{
+		return new A;
+		break;
+	}
+	case 1:
+	{
+		return new B;
+		break;
+	}
+	case 2:
+	{
+		return new C;
+		break;
+	}
+	}
+	return new A;
 }
 
 void identify(Base* p)
