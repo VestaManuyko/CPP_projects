7f6103a4e455f4dd4db89a36802fa86ac934343d
Vesta Manuyko <Vesta.manuyko@gmail.com>
2026-08-31 12:33:47 +0200

    first templates done

--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -4,19 +4,21 @@
 template<typename T>
 void swap(T &x, T& y)
 {
-
+	T	tmp = x;
+	x = y;
+	y = tmp;
 }
 
 template<typename T>
 const T	&min(const T &x, const T &y)
 {
-
+	return (x < y) ? x : y;
 }
 
 template<typename T>
 const T	&max(const T &x, const T &y)
 {
-
+	return (x > y) ? x : y;
 }
 
 #endif
