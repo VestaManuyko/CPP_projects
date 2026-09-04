9a43d26c5d81b1131b17e1d3ea85010826523426
Vesta Manuyko <Vesta.manuyko@gmail.com>
2026-09-04 13:50:30 +0200

    added necessary attributes

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,11 +1,16 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+template<typename T>
 class Array
 {
+	private:
+		T *_array;
+
 	public:
     	Array();
-		Array(const Array& other); 
+		Array(unsigned int n);
+		Array(const Array& other);
 		Array& operator=(const Array& other);
 		~Array();
 };
